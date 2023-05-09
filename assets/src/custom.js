384eefa95c456c89ab12ab9c7d26a33b9c5c3b6e
Alex <dafen@ukr.net>
2023-05-09 09:15:24 +0300

    init: init task

---
+++ assets/src/custom.js
@@ -0,0 +1,73 @@
+const element = document.querySelector('form');
+const commentArea = document.querySelector('#userComment');
+const commentsContainer = document.querySelector('.comments_face');
+
+element.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log('Form submission cancelled.', commentArea.value);
+
+  let comment = document.createElement('div')
+  comment.className = 'comments'
+
+  comment.innerHTML = `
+      <div class="profile">
+        <img src="./assets/img/anonymous-user.png"></div>
+      <div class="comment-content">
+        <p class="name">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Usuário anônimo</font>
+          </font>
+        </p>
+        <p>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">${commentArea.value}</font>
+          </font>
+        </p>
+      </div>
+      <div class="clr"></div>
+      <div class="comment-status">
+        <span>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Curte·comente</font>
+          </font>
+          <img src="./assets/img/like.png" width="15px" height="15px" onClick='likeHandler()'>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">0</font>
+          </font>
+        </span>
+        <small>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">·</font>
+          </font>
+          <u>
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">0 minutos antes</font>
+            </font>
+          </u>
+        </small>
+      </div>
+  `
+  commentsContainer.insertBefore(comment, commentsContainer.children[1]);
+
+  commentArea.value = '';
+});
+
+const questions = document.querySelector('.q-container');
+let answersArr = []
+questions.addEventListener('click', function(event) {
+
+  if (event.target.classList.contains('survey_button')) {
+    console.log('good');
+    answersArr.push(event.target.childNodes[1].childNodes[1].textContent);
+    console.log(answersArr);
+  } else {
+    console.log('bad');
+    answersArr.push(event.target.textContent);
+    console.log(answersArr);
+  }
+});
+
+function okHandler () {
+  console.log('Respostas do usuário:');
+  answersArr.forEach(x => console.log(x));
+}
